d022542554b4f62390e0fb0fb3fc3e34acd44d9d
Micheal Nestor <michealnestor@outlook.com>
2025-04-26 15:36:03 +0100

    fixed cors issue

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,6 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173/"],
     allow_credentials=True,
-    allow_methods=True,
     allow_methods=["*"],
     allow_headers=["*"]
 )
