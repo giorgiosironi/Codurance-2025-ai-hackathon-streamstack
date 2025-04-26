c04f196e4c3922021fa667356e9454806a2da874
Micheal Nestor <michealnestor@outlook.com>
2025-04-26 15:33:23 +0100

    added cors middleware

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import pandas as pd
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173/"],
+    allow_credentials=True,
+    allow_methods=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 NETFLIX_TITLES = pd.read_csv("netflix_titles.csv")
 
