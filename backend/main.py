e2f3e400bec2958a3fa540aec374721a604b873a
Isaac Oldwood <Isaac@mulberryrisk.com>
2025-04-26 15:14:09 +0100

    Refactor out chosen filter

--- backend/main.py
+++ backend/main.py
@@ -3,18 +3,22 @@ import pandas as pd
 
 app = FastAPI()
 
-NETFLIX_TITLES = pd.read_csv('netflix_titles.csv')
+NETFLIX_TITLES = pd.read_csv("netflix_titles.csv")
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+
 @app.get("/titles")
-async def titles(title: str = '', director: str = ''):
+async def titles(title: str = "", director: str = ""):
+    chosen_filter = pd.Series(True, index=range(0, len(NETFLIX_TITLES)))
+
     if title:
-        title_filter = NETFLIX_TITLES['title'].str.contains(title)
-        return NETFLIX_TITLES[title_filter].fillna('').to_dict('records')
+        chosen_filter = NETFLIX_TITLES["title"].str.contains(title)
+
     if director:
-        director_filter = NETFLIX_TITLES['director'].fillna('').str.contains(director)
-        return NETFLIX_TITLES[director_filter].fillna('').to_dict('records')
-    return NETFLIX_TITLES.fillna('').to_dict('records')
+        chosen_filter = NETFLIX_TITLES["director"].fillna("").str.contains(director)
+
+    return NETFLIX_TITLES[chosen_filter].fillna("").to_dict("records")
