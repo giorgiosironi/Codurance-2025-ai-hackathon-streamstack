0e9bc2f271b849bff025bce394bd211b95e26641
Micheal Nestor <michealnestor@outlook.com>
2025-04-26 15:19:11 +0100

    added title frame

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import "./App.css";
+import TitleFrame from "./components/TitleFrame";
 
 function App() {
   return (
-    <div className="bg-black h-screen w-full flex justify-center items-center">
-      <h1 className="text-3xl text-white">Vite + React + TS + Testing Template</h1>
+    <div className="bg-stone-950 h-screen w-full flex flex-col justify-start items-center">
+      <h1 className="text-5xl text-white p-24">Stream Stack</h1>
+      <div className="text-white p-8 flex flex-row items-center justify-center">
+        Filter Bar
+      </div>
+      <TitleFrame
+        title="Jurasic World Rebirth"
+        type="Movie"
+        year={2025}
+        imgUrl="https://i.ytimg.com/vi/jan5CFWs9ic/maxresdefault.jpg"
+      />
     </div>
   );
 }
---
+++ frontend/src/components/TitleFrame.tsx
@@ -0,0 +1,27 @@
+interface TitleFrameProps {
+  title: string;
+  type: string;
+  year: number;
+  imgUrl: string;
+}
+
+const TitleFrame = (props: TitleFrameProps) => {
+  return (
+    <div className="w-96 h-80 flex flex-col items-center shadow-[3px_3px_15px_4px_rgba(100,_100,_111,_0.2)] cursor-pointer text-white hover:scale-105 active:scale-95 transform transition duration-800 ease-in-out">
+      <img
+        src={props.imgUrl}
+        alt="title frame image"
+        className="max-h-1/2 max-w-full"
+      />
+      <div className="flex flex-col items-center justify-evenly w-full h-full">
+        <h1 className="text-3xl">{props.title}</h1>
+        <div className="flex flex-row justify-evenly w-full">
+          <h3 className="text-xl">{props.type}</h3>
+          <h3 className="text-xl">{props.year}</h3>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TitleFrame;
